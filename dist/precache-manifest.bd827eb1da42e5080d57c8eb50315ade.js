4f945d1492137719829bdb7bfa437d6b97c7f1f9
vanhock <vanhocker@gmail.com>
2019-01-31 19:21:07 +0300

    update build

---
+++ dist/precache-manifest.bd827eb1da42e5080d57c8eb50315ade.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc3b8dca0f2b394a620f",
+    "url": "/js/chunk-vendors.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "de9e6b0b66a37161f814",
+    "url": "/js/login.js"
+  },
+  {
+    "revision": "a2add9f1172747d11d63",
+    "url": "/css/auth.css"
+  },
+  {
+    "revision": "dc4629bf80e94bbaad9c",
+    "url": "/css/auth~login~registration.css"
+  },
+  {
+    "revision": "dc4629bf80e94bbaad9c",
+    "url": "/js/auth~login~registration.js"
+  },
+  {
+    "revision": "99b8c2e77008c77cb50b",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "a2add9f1172747d11d63",
+    "url": "/js/auth.js"
+  },
+  {
+    "revision": "d32d302fc12cb1f7e449",
+    "url": "/css/registration.css"
+  },
+  {
+    "revision": "d32d302fc12cb1f7e449",
+    "url": "/js/registration.js"
+  },
+  {
+    "revision": "d846d67b8269b658a9e0fc0c3ef3d47a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0442272e09b8468351f9eda619473f16",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "99b8c2e77008c77cb50b",
+    "url": "/css/app.css"
+  }
+];
