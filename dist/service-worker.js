00c71b0351673351faa5ef24ca86c41252ff7ccb
vanhock <vanhocker@gmail.com>
2019-01-31 19:15:41 +0300

    update build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ecd831789ccefb6dc9da785e74ab82f1.js"
+  "precache-manifest.45383c688122efe55da9beadbdaeef9b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "bleach"});
